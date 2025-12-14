b0097f4aade6fe309bffce598425695d4626ec0c
samudranilsen-hub <samudranil.sen@jove.com>
2025-12-14 19:02:15 +0530

    Update result.js

--- api/result.js
+++ api/result.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Use /api/generate to get results" });
+  res.status(200).json({ message: "Use /api/generate directly." });
 }
